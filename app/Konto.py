86dd07efbb77de304fe820d06b777cba0ca62037
Gracjan Koscinski <gracek247927@gmail.com>
2023-10-16 10:21:06 +0200

    Feature 5

--- app/Konto.py
+++ app/Konto.py
@@ -7,10 +7,23 @@ class Konto:
             self.pesel="Niepoprawny pesel!"
         else:
             self.pesel = pesel
-        if self.czy_kod_promocyjny_poprawny(kod_promocyjny):
-            self.saldo = 50
+
+        #jeżeli urodzony po 1960 to sprawdz czy dobry kod
+        if self.pesel != "Niepoprawny pesel!":
+            if self.czy_urodzony_po_1960(pesel) and self.czy_kod_promocyjny_poprawny(kod_promocyjny):
+                self.saldo = 50
     
     def czy_kod_promocyjny_poprawny(self, kod_promocyjny):
         if kod_promocyjny == None:
             return False
         return len(kod_promocyjny) == 8 and kod_promocyjny[:5] == "PROM_"
+    
+    def czy_urodzony_po_1960(self,pesel):
+        if int(pesel[2:4]) > 12 and int(pesel[2:4]) < 81:
+            return True
+            #print("Osoba urodzona po 2000 roku")
+        elif int(pesel[2:4]) < 13 and int(pesel[:2]) > 60:
+            return True
+            #print("Osoba urodzona między 1960 (nie włącznie) a 2000 rokiem")
+        else:
+            return False
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -27,20 +27,47 @@ class TestCreateBankAccount(unittest.TestCase):
     
     def test_tworzenie_konta_z_niepoprawnym_kodem_promocyjnym(self):
         konto = Konto(self.imie, self.nazwisko, self.pesel, "PROM_12345")
-        self.assertEqual(konto.saldo, 0, "kod zostal zaakceptowany!")
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
     
     def test_tworzenie_konta_z_niepoprawnym_kodem_promocyjnym_preffix(self):
         konto = Konto(self.imie, self.nazwisko, self.pesel, "TEST_XYZ")
-        self.assertEqual(konto.saldo, 0, "kod zostal zaakceptowany!")
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
     
     def test_osoba_urodzona_w_1970(self):
+        konto = Konto(self.imie, self.nazwisko, "70010112345", "PROM_XYZ")
+        self.assertEqual(konto.saldo, 50, "kod nie został zaakceptowany")
     
-    def test_osoba_urodzona_w_2003(self):
+    def test_osoba_urodzona_w_1970_zly_kod(self):
+        konto = Konto(self.imie, self.nazwisko, "70010112345", "ZLY_KOD")
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
+    
+    def test_osoba_urodzona_w_1970_zly_pesel(self):
+        konto = Konto(self.imie, self.nazwisko,"700101", "PROM_XYZ" )
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
 
     def test_osoba_urodzona_w_1960(self):
-    
-    def test_osoba_zly_pesel(self):
+        konto = Konto(self.imie, self.nazwisko, "60010112345", "PROM_XYZ")
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
 
-    def test_osoba_urodzona_w_1970_zly_kod(self):
+    def test_osoba_urodzona_w_1960_zly_kod(self):
+        konto = Konto(self.imie, self.nazwisko, "60010112345", "ZLY_KOD")
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
+
+    def test_osoba_urodzona_w_1960_zly_pesel(self):
+        konto = Konto(self.imie, self.nazwisko,"60010112345333", "PROM_XYZ" )
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
+    
+    def test_osoba_urodzona_w_2003(self):
+        konto = Konto(self.imie, self.nazwisko, "03310112345", "PROM_XYZ")
+        self.assertEqual(konto.saldo, 50, "kod nie został zaakceptowany")
+    
+    def test_osoba_urodzona_w_2003_zly_kod(self):
+        konto = Konto(self.imie, self.nazwisko,"03311801475", "ZLY_KOD" )
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
+    
+    def test_osoba_urodzona_w_2003_zly_pesel(self):
+        konto = Konto(self.imie, self.nazwisko,"0333333333333333333333", "PROM_XYZ" )
+        self.assertEqual(konto.saldo, 0, "kod został zaakceptowany(nie powinien zostać zaakceptowany)")
+
 
     #tutaj proszę dodawać nowe testy
