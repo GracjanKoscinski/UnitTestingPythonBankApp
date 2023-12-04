1da687a084a51c9dac5a3847ef7f9d2bec282492
Gracjan Koscinski <gracek247927@gmail.com>
2023-12-04 17:08:21 +0100

    Feature 18

--- app/tests/test_create_company_account.py
+++ app/tests/test_create_company_account.py
@@ -1,16 +1,20 @@
 import unittest
 
 from ..KontoFirmowe import KontoFirmowe
-
+from unittest.mock import patch
+@patch('app.KontoFirmowe.KontoFirmowe.czy_w_rejestrze')
 class TestCreateBankAccount(unittest.TestCase):
     name = "Firma123"
-    def test_tworzenie_konta(self):
-        nip = "1234567890"
+    def test_tworzenie_konta(self,czy_w_rejestrze):
+        czy_w_rejestrze.return_value = True
+        nip = "8461627563"
         pierwsze_konto = KontoFirmowe(self.name,nip)
         self.assertEqual(pierwsze_konto.name,self.name, "Nazwa nie została zapisana!")
         self.assertEqual(pierwsze_konto.nip,nip, "Nip nie został zapisany!")
     
-    def test_tworzenie_konta_zly_nip(self):
-        nip = "33"
-        pierwsze_konto = KontoFirmowe(self.name,nip)
-        self.assertEqual(pierwsze_konto.nip,"Niepoprawny NIP!","nip nie zostal zapisany")
+    def test_tworzenie_konta_z_nieistniejacym_nip(self,czy_w_rejestrze):
+        nip = "1234567891"
+        czy_w_rejestrze.return_value = False
+        with self.assertRaises(Exception) as context:
+            konto = KontoFirmowe(self.name,nip)
+            self.assertTrue("This NIP does not exist" in str(context.exception))
--- app/tests/test_transfers_history.py
+++ app/tests/test_transfers_history.py
@@ -2,37 +2,41 @@ import unittest
 
 from ..KontoOsobiste import KontoOsobiste
 from ..KontoFirmowe import KontoFirmowe
-
+from unittest.mock import patch
+@patch('app.KontoFirmowe.KontoFirmowe.czy_w_rejestrze')
 class TestBankAccountTransfersHistory(unittest.TestCase):
 
-    def test_historia_konto_osobiste_przychodzacy(self):
+    def test_historia_konto_osobiste_przychodzacy(self,czy_w_rejestrze):
         konto_1 = KontoOsobiste("imie","nazwisko","10000000000")
         konto_1.saldo = 0
         konto_1.przelew_przychodzacy(100)
         self.assertEqual(konto_1.historia, [100], "historia niepoprawna!")
-    def test_historia_konto_osobiste_wychodzacy(self):
+    def test_historia_konto_osobiste_wychodzacy(self,czy_w_rejestrze):
         konto_1 = KontoOsobiste("imie","nazwisko","10000000000")
         konto_1.saldo = 100
         konto_1.przelew_wychodzacy(50)
         self.assertEqual(konto_1.historia, [-50], "historia niepoprawna!")
-    def test_historia_konto_osobiste_express(self):
+    def test_historia_konto_osobiste_express(self,czy_w_rejestrze):
         konto_1 = KontoOsobiste("imie","nazwisko","10000000000")
         konto_1.saldo = 300
         konto_1.przelew_express_wychodzacy(100)
         self.assertEqual(konto_1.historia, [-100,-1], "historia niepoprawna!")
     #konto firmowe
-    def test_historia_konto_firma_przychodzacy(self):
-        konto_1 = KontoFirmowe("nazwa","1234567890")
+    def test_historia_konto_firma_przychodzacy(self,czy_w_rejestrze):
+        czy_w_rejestrze.return_value = True
+        konto_1 = KontoFirmowe("nazwa","8461627563")
         konto_1.saldo = 100
         konto_1.przelew_przychodzacy(50)
         self.assertEqual(konto_1.historia, [50], "saldo niepoprawne!")
-    def test_historia_konto_firma_wychodzacy(self):
-        konto_1 = KontoFirmowe("nazwa","1234567890")
+    def test_historia_konto_firma_wychodzacy(self,czy_w_rejestrze):
+        czy_w_rejestrze.return_value = True
+        konto_1 = KontoFirmowe("nazwa","8461627563")
         konto_1.saldo = 100
         konto_1.przelew_wychodzacy(50)
         self.assertEqual(konto_1.historia, [-50], "saldo niepoprawne!")
-    def test_historia_konto_firma_express(self):
-        konto_1 = KontoFirmowe("nazwa","1234567890")
+    def test_historia_konto_firma_express(self,czy_w_rejestrze):
+        czy_w_rejestrze.return_value = True
+        konto_1 = KontoFirmowe("nazwa","8461627563")
         konto_1.saldo = 100
         konto_1.przelew_express_wychodzacy(50)
         self.assertEqual(konto_1.historia, [-50,-5], "saldo niepoprawne!")
