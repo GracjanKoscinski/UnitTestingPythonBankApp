9e145a416baf4ef4e15173566b27378873a50505
Gracjan Kościński <gkoscinski@inf.ug.edu.pl>
2023-12-18 17:10:04 +0100

    feature 20 TODO

--- app/RegisterOfAccounts.py
+++ app/RegisterOfAccounts.py
@@ -1,6 +1,9 @@
 from .KontoOsobiste import KontoOsobiste
-
+from pymongo import MongoClient
 class RegisterOfAccounts:
+    client = MongoClient('localhost', 27017)
+    db = client['mydatabase']
+    collection = db['konta']
     register = []
 
     @classmethod
@@ -18,3 +21,14 @@ class RegisterOfAccounts:
             if konto.pesel==pesel:
                 return konto
         return None
+    
+    @classmethod
+    def saveToDatabase(cls):
+        cls.collection.delete_many({})
+        for konto in cls.register:
+            cls.collection.insert_one({"imie:": konto.imie, "nazwisko": konto.nazwisko, "pesel": konto.pesel, "saldo": konto.saldo, "historia:": konto.historia})
+    
+    @classmethod
+    def loadFromDatabase(cls):
+        for konto in cls.collection.find():
+            cls.register.append(KontoOsobiste(konto["imie"], konto["nazwisko"], konto["pesel"], konto["saldo"], konto["historia"]))
